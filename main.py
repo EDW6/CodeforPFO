37c9cdec7f26b4673943e0aed0f723d77110e533
WANGHONGJIAN <whj00529>
2022-04-26 11:35:49 +0800

    Note

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import os
 import scipy.signal as signal
 import struct
 import matplotlib.pyplot as plt
@@ -17,7 +18,9 @@ def get_from_bin(filepath_input):
     for i in range(byt_size):
         val = struct.unpack('H', f.read(2))[0]
         lst.append(val-32700)
+    lst = np.array(lst)
     return lst
+
 
 def gets_from_bin(filepath_input):
     lst = get_from_bin(filepath_input)
@@ -25,6 +28,7 @@ def gets_from_bin(filepath_input):
     en1 = lst.reshape(lst.size)
     lst_s_temp = signal.medfilt(en1, kernel_size=401)
     lst_s = lst - lst_s_temp
+    lst_s = np.array(lst_s)
     return lst_s
 
 
@@ -38,22 +42,29 @@ def fun_diff(data):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    base_root = r'F:\2022多模态\0322测试'
-    object_name = '纤细裸藻光语/23-19-27-0度线偏振//'
+    base_root = r'F:\2022多模态\0322测试\纤细裸藻光语'
     ch_name = ['ch2.bin', 'ch3.bin', 'ch4.bin', 'ch5.bin', 'ch1.bin']
 
-    filepath = base_root + '/' + object_name + ch_name[0]
-    dataleft1 = get_from_bin(filepath)
-    filepath = base_root + '/' + object_name + ch_name[1]
-    data0 = get_from_bin(filepath)
-    filepath = base_root + '/' + object_name + ch_name[2]
-    data90 = get_from_bin(filepath)
-    filepath = base_root + '/' + object_name + ch_name[3]
-    data45 = get_from_bin(filepath)
-    filepath = base_root + '/' + object_name + ch_name[4]
-    dataf = get_from_bin(filepath)
+    object_all = os.listdir(base_root)
+    len_datafile = len(object_all)
+    # len_datafile - 1
+    for list_num in range(2):
+        object_name = object_all[list_num+1]
 
-    data_optical_all = dataleft1 + data0 + data90 + data45 + dataf
+        for ch_num in ch_name:
+            filepath = os.path.join(base_root, object_name, str(ch_num))
+            data_for_ch = get_from_bin(filepath)
+
+            os.chdir(base_root + '//' + object_name)
+            store_name = ch_num[:3] + '.npy'
+            np.save(file=store_name, arr=data_for_ch)
+
+
+
+
+
+
+    data_optical_all = data1 + data0 + data90 + data45 + dataf
 
     # fig1 = plt.figure(num='fig111111')
     # plt.plot(data_optical_all)
@@ -62,14 +73,22 @@ if __name__ == '__main__':
     data_o_diff = fun_diff(data_optical_all)
 
     fig2 = plt.figure(num='fig121111')
-    plt.plot(data_o_diff)
-    plt.show()
+    # plt.plot(data_o_diff)
+    # plt.show()
 
     filepath_s = base_root + '/' + object_name + 'ch8.bin'
     datas = gets_from_bin(filepath_s)
 
+    data_to_store = np.concatenate((data1, data0, data90, data45), axis=0)
+    print(data_to_store.shape)
+    print(data1.shape)
+
     # 找peaks
-    peak_id, peak_property = signal.find_peaks(datas, height=200, distance=20)
+    peak_id, peak_property = signal.find_peaks(datas, height=100, distance=200)
+    #
+    #
+    # plt.plot(datas)
+    # plt.show()
 
 
 
