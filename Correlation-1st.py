6e828d3fe00135f5b0b5b39068803a7c48bfdb7c
WANGHONGJIAN <whj00529>
2022-04-28 16:03:47 +0800

    Note

--- Correlation-1st.py
+++
@@ -1,6 +0,0 @@
-import os
-import numpy as np
-import matplotlib.pyplot as plt
-from scipy import signal
-from scipy.fftpack import fft
-
---
+++ __init__.py
@@ -0,0 +1,7 @@
+# init文件，将文件夹变成一个模块，在这里可以导入我们需要的模块，其他的地方不用导入
+# __init__.py
+
+import sys
+import Preprocessing as Pre
+
+print(sys.path)
