21447f1dde66559d629b79f7ba43fcec1005ae96
WANGHONGJIAN <whj00529>
2022-07-07 23:04:00 +0800

    Note

--- GenerateNPY.py
+++ GenerateNPY.py
@@ -1,23 +1,24 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+#!/usr/bin/env/ python3
+# 22-05-03
+# 采集完成后的第一步
+# 用于生成npy文件
 import os
-import scipy.signal as signal
-import struct
-import matplotlib.pyplot as plt
 import numpy as np
+import struct
+
+from scipy import signal
 
 
 def get_from_bin(filepath_input):
-    f = open(filepath_input, 'rb')
+    print(filepath_input)
+    f = open(filepath_input,'rb')
     byt = f.read()
     byt_size = int(len(byt) / 2) - 1
     f = open(filepath_input, 'rb')
     lst = []
     for i in range(byt_size):
         val = struct.unpack('H', f.read(2))[0]
-        lst.append(val-32700)
+        lst.append(val - 32700)
     lst = np.array(lst)
     return lst
 
@@ -25,66 +26,90 @@ def get_from_bin(filepath_input):
 def gets_from_bin(filepath_input):
     lst = get_from_bin(filepath_input)
     lst = np.array(lst)
-    en1 = lst.reshape(lst.size)
-    lst_s_temp = signal.medfilt(en1, kernel_size=401)
+
+    lst_s_temp = np.convolve(lst, np.ones((400,)) / 400, mode='same')  # signal.medfilt(lst, kernel_size=401)
     lst_s = lst - lst_s_temp
-    lst_s = np.array(lst_s)
+    lst_s[:400] = 0
+    lst_s[len(lst_s) - 400:] = 0
+
+    # lst_s = np.array(lst_s)
     return lst_s
 
 
+class Bin2Num(object):
+    def __init__(self,  base_root0, name_list): # self参数是类下面所有方法必须的参数
+        self.base_root0 = base_root0
+        self.name_list = name_list
+        pass
+
+    # 光脉冲的提取
+
+    # 声音脉冲的提取
+    def generate_number_from_bin(self):
+        for name_index in range(len(self.name_list)):
+            print(self.name_list)
+            base_root = self.base_root0 + self.name_list[name_index]
+            ch_name = ['ch2.bin', 'ch3.bin', 'ch4.bin', 'ch5.bin', 'ch1.bin']
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    base_root = r'F:\2022多模态\0322测试\裸藻-状态可能不好了-水生所的'
-    ch_name = ['ch2.bin', 'ch3.bin', 'ch4.bin', 'ch5.bin', 'ch1.bin']
+            object_all = os.listdir(base_root)
+            len_datafile = len(object_all)-1
 
-    object_all = os.listdir(base_root)
-    len_datafile = len(object_all)
-    # len_datafile - 1
-    for list_num in range(len_datafile - 1):
-        object_name = object_all[list_num+1]
-        print(object_name)
+            for list_num in range(len_datafile):
+                object_name = object_all[list_num+1]
+                print(object_name)
+                print('processing --> %d/%d' % (list_num, len_datafile))
 
-        for ch_num in ch_name:
-            filepath = os.path.join(base_root, object_name, str(ch_num))
-            data_for_ch = get_from_bin(filepath)
+                os.chdir(os.path.join(base_root, object_name))
+                if os.path.exists('ch4.npy'):
+                    print('pass')
+                else:
+                    for ch_num in ch_name:
+                        filepath = os.path.join(base_root, object_name, str(ch_num))
+                        data_for_ch = get_from_bin(filepath)
 
-            os.chdir(base_root + '//' + object_name)
-            store_name = ch_num[:3] + '.npy'
-            np.save(file=store_name, arr=data_for_ch)
+                        os.chdir(base_root + '//' + object_name)
+                        store_name = ch_num[:3] + '.npy'
+                        np.save(file=store_name, arr=data_for_ch)
 
-            print('done')
+                    filepath_s = base_root + '/' + object_name + '/ch8.bin'
+                    datas = gets_from_bin(filepath_s)
+                    np.save(file='ch8.npy', arr=datas)
+                    print('done')
 
-        filepath_s = base_root + '/' + object_name + '/ch8.bin'
-        datas = gets_from_bin(filepath_s)
-        print('done')
-        np.save(file='ch8.npy', arr=datas)
-
-    print('全部生成完毕')
-
-    # data_optical_all = data1 + data0 + data90 + data45 + dataf
-
-    # fig1 = plt.figure(num='fig111111')
-    # plt.plot(data_optical_all)
-    # plt.show()
-    #
-    # data_o_diff = fun_diff(data_optical_all)
-    #
-    # fig2 = plt.figure(num='fig121111')
-    # plt.plot(data_o_diff)
-    # plt.show()
+            print('全部生成完毕')
 
 
+    def make_whole_from_separate(self):
+        ch_name = ['ch2.npy', 'ch3.npy', 'ch4.npy', 'ch5.npy', 'ch1.npy', 'ch8.npy']
 
-    # data_to_store = np.concatenate((data1, data0, data90, data45), axis=0)
-    # print(data_to_store.shape)
-    # print(data1.shape)
+        for name_index in range(len(self.name_list)):
+            print(self.name_list[name_index])
+            base_root = self.base_root0 + self.name_list[name_index]
 
+            os.chdir(base_root)
+            if os.path.exists('ch4.npy'):
+                pass
+            else:
+                sampling_file = os.listdir(base_root)
 
-    #
-    # plt.plot(datas)
-    # plt.show()
+                len_sampling = 11  # len(sampling_file)-2
 
+                os.chdir(base_root)
 
+                for ch_index in ch_name:
+                    print(ch_index)
+                    data2store = []
+                    for list_sample in range(2, len_sampling):
+                        sample_file_name = sampling_file[list_sample]
+                        total_path = os.path.join(base_root, sample_file_name, ch_index)
+                        # 用于把全部采样的通道进行叠加
+                        data_for_ch = np.load(total_path)
+                        data2store = np.hstack([data2store, data_for_ch])
+                        print(len(data2store))
+                    print('done')
+                    os.chdir(base_root)
+                    np.save(file=ch_index, arr=data2store)
+
+        print('Please continue')
 
--- __init__.py
+++ __init__.py
@@ -1,6 +1,2 @@
 # init文件，将文件夹变成一个模块，在这里可以导入我们需要的模块，其他的地方不用导入
 # __init__.py
-
-
-import Preprocessing.GenerateNPY
-
